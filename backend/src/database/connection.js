ffca8c9fb544bf0b9f7f4fe20da5c3ead2e9d69f
pauloalchemist <paulolinsdev@gmail.com>
2020-12-24 17:01:42 -0300

    connection db

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,7 +1,5 @@
 const { Client } = require('pg');
-require('dotenv').config({
-	path: '../../../.env',
-});
+require('dotenv').config();
 
 const db = new Client({
 	connectionString: process.env.DATABASE_URL,
