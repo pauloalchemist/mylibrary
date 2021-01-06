7aa99cc6f599e97c4c9ea43fad8bb20b255ba51e
pauloalchemist <paulolinsdev@gmail.com>
2021-01-06 16:42:07 -0300

    modified with standardjs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const app = express();
-const routes = require('./routes.js');
-const PORT = process.env.PORT || 8080;
-require('dotenv').config();
+const express = require('express')
+const app = express()
+const routes = require('./routes.js')
+const PORT = process.env.PORT || 8080
+require('dotenv').config()
 
-app.use(express.json());
-app.use(routes);
+app.use(express.json())
+app.use(routes)
 
 app.listen(PORT, () => {
-	console.log(`Servidor rodando na porta:`, PORT);
-});
+  console.log('Servidor rodando na porta:', PORT)
+})
