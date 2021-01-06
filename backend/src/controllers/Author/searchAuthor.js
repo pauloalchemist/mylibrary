0ef1115d2f1df5032588933eefdb5a70c5492ecf
pauloalchemist <paulolinsdev@gmail.com>
2021-01-06 16:59:06 -0300

    modified with standardjs and included text search in the query

--- backend/src/controllers/Author/searchAuthor.js
+++ backend/src/controllers/Author/searchAuthor.js
@@ -1,20 +1,19 @@
-const db = require('../../database/connection.js');
+const db = require('../../database/connection.js')
 
 module.exports = {
-	async searchAuthor(req, res) {
-		try {
-			const { author_name } = req.body;
+  async searchAuthor (req, res) {
+    try {
+      const { authorName } = req.body
 
-			const { rows } = await db.query(
-				`
-				SELECT * FROM authors WHERE author LIKE '%${author_name}%' ORDER BY author;
-				`
-			);
+      const { rows } = await db.query(
+        'SELECT id, author FROM authors WHERE to_tsvector(author) @@ to_tsquery($1) ORDER BY author;',
+        [authorName]
+      )
 
-			return res.status(200).json({ Authors: rows });
-		} catch (error) {
-			console.error(error);
-			return res.status(400).json(error.errors);
-		}
-	},
-};
+      return res.status(200).json({ Authors: rows })
+    } catch (error) {
+      console.error(error)
+      return res.status(400).json(error.errors)
+    }
+  }
+}
